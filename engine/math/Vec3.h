95cfb8d51f327459139d821d8c1f284d56eb01ac
Andrew Weller <andrewweller.cs@gmail.com>
2019-09-25 15:42:51 -0600

    Vec3: fix compilation errors

--- engine/math/Vec3.h
+++ engine/math/Vec3.h
@@ -14,6 +14,8 @@ public:
     Vec3  operator -(const Vec3& other) const;
     Vec3  operator *(float scalar) const;
     Vec3  operator /(float scalar) const;
+    
+    float operator[](int index);
 
     float magnitude();
     float magnitude_squared();
@@ -29,74 +31,3 @@ public:
 
     float x, y, z;
 };
-
-Vec3::Vec3(float x, float y, float z) {
-    this-> x = x;
-    this-> y = y;
-    this-> z = z;
-}
-
-Vec3& Vec3::operator =(const Vec3& other) {
-    x = other.x;
-    y = other.y;
-    z = other.z;
-    return *this;
-}
-
-Vec3 Vec3::operator +(const Vec3& other) const {
-    return Vec3(
-        x + other.x,
-        y + other.y,
-        z + other.z
-    );
-}
-
-Vec3 Vec3::operator -(const Vec3& other) const {
-    return Vec3(
-        x - other.x,
-        y - other.y, 
-        z - other.z
-    );
-}
-
-Vec3 Vec3::operator *(float scalar) const {
-    return Vec3(
-        x * scalar,
-        y * scalar,
-        z * scalar
-    );
-}
-
-Vec3 Vec3::operator /(float scalar) const {
-    return Vec3(
-        x / scalar,
-        y / scalar,
-        z / scalar
-    );
-}
-
-float Vec3::magnitude() {
-    return sqrt(magnitude_squared());
-}
-
-float Vec3::magnitude_squared() {
-    return dot(*this, *this);
-}
-
-Vec3 Vec3::normalize() {
-    return *this / magnitude();
-}
-
-float Vec3::dot(const Vec3& first, const Vec3& second) {
-    return first.x * second.x 
-         + first.y * second.y 
-         + first.z * second.z;
-}
-
-Vec3 Vec3::cross(const Vec3& first, const Vec3& second) {
-    return Vec3(
-        first.y * second.z - first.z * second.y,
-        first.z * second.x - first.x * second.z,
-        first.x * second.y - first.y * second.x
-    );
-}
