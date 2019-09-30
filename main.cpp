705698eafec90788f7fef9d78a3f36a2f506cd4e
Andrew Weller <andrewweller.cs@gmail.com>
2019-09-30 00:13:04 -0600

    Main: test camera and triangle transforms

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
     sf::RenderWindow window(sf::VideoMode(800,600), "bhop_world", sf::Style::Titlebar);
     window.setPosition(sf::Vector2i(400, 150));
 
@@ -13,16 +12,55 @@ int main() {
         bh::Vec2(600.0f, 0.0f)
     );
 
+    bh::Tri3 weird_triangle {
+        bh::Vec3(0.0f, 0.0f, 7.0f),
+        bh::Vec3(-1.0f, 5.0f, 12.0f),
+        bh::Vec3(-2.0f, -3.0f, 4.0f)
+    };
+
+    bh::Camera cam;
+
+    bh::Mat3 rotation = bh::Mat3::rotation_around_y(0.000001);
+
+    sf::Time dt;
+    sf::Clock deltaClock;
+    deltaClock.restart();
     while(window.isOpen()) {
+        dt = deltaClock.restart();
+        bh::Mat4 translation = bh::Mat4::translate(0.7*dt.asSeconds(), 0, 0);
+
         sf::Event event;
         while(window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+            if (event.type == sf::Event::KeyPressed) {
+				switch(event.key.code)
+				{
+					case sf::Keyboard::Key::Right:
+				        cam.rotate(0, 0.1);
+					break;
+                    case sf::Keyboard::Key::Left:
+				        cam.rotate(0, -0.1);
+					break;
+                    case sf::Keyboard::Key::Up:
+				        cam.rotate(0.1, 0);
+					break;
+                    case sf::Keyboard::Key::Down:
+				        cam.rotate(-0.1, 0);
+					break;
+                }
+            }
         }
+        
+        //weird_triangle.c.z += 0.001;
+        weird_triangle.apply_transform(rotation);
+        //weird_triangle.apply_transform(translation);
+        //cam.rotate(0.000001, -0.000001);
+        bh::Tri2 projection = bh::Camera::project_triangle(cam, weird_triangle);
 
         window.clear();
-        bh::Tri2::draw_to_screen(triangle, window);
+        bh::Tri2::draw_to_screen(projection, window);
         window.display();
     }
 	return 0;
