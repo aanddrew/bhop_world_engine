1cc24a625d7a2f7b5a52363a3f04a04980f27bcc
Andrew Weller <andrewweller.cs@gmail.com>
2019-09-29 22:44:44 -0600

    Vec4: creation

---
+++ engine/math/Vec4.cpp
@@ -0,0 +1,136 @@
+#include "Vec4.h"
+
+namespace bh {
+
+Vec4::Vec4(float x, float y, float z, float w) {
+    this-> x = x;
+    this-> y = y;
+    this-> z = z;
+    this-> w = w;
+}
+
+Vec4::Vec4(const Vec3& other, float w) {
+    this->x = other.x;
+    this->y = other.y;
+    this->z = other.z;
+    this->w = w;
+}
+
+Vec4::Vec4(const Vec3& other)
+: Vec4(other, 1)
+{}
+
+Vec4::Vec4()
+: Vec4(0, 0, 0, 0)
+{}
+
+Vec4& Vec4::operator =(const Vec4& other) {
+    x = other.x;
+    y = other.y;
+    z = other.z;
+    w = other.w;
+    return *this;
+}
+
+Vec4& Vec4::operator =(const Vec3& other) {
+    x = other.x;
+    y = other.y;
+    z = other.z;
+    w = 1;
+    return *this;
+}
+
+void Vec4::operator +=(const Vec4& other) {
+    x += other.x;
+    y += other.y;
+    z += other.z;
+    w += other.w;
+}
+
+void Vec4::operator -=(const Vec4& other) {
+    x -= other.x;
+    y -= other.y;
+    z -= other.z;
+    w -= other.w;
+}
+
+void Vec4::operator *=(float scalar) {
+    x *= scalar;
+    y *= scalar;
+    z *= scalar;
+    w *= scalar;
+}
+
+void Vec4::operator /=(float scalar) {
+    x /= scalar;
+    y /= scalar;
+    z /= scalar;
+    w /= scalar;
+}
+
+Vec4 Vec4::operator +(const Vec4& other) const {
+    return Vec4(
+        x + other.x,
+        y + other.y,
+        z + other.z,
+        w + other.w
+    );
+}
+
+Vec4 Vec4::operator -(const Vec4& other) const {
+    return Vec4(
+        x - other.x,
+        y - other.y, 
+        z - other.z,
+        w - other.w
+    );
+}
+
+Vec4 Vec4::operator *(float scalar) const {
+    return Vec4(
+        x * scalar,
+        y * scalar,
+        z * scalar,
+        w * scalar
+    );
+}
+
+Vec4 Vec4::operator /(float scalar) const {
+    return Vec4(
+        x / scalar,
+        y / scalar,
+        z / scalar,
+        w / scalar
+    );
+}
+
+float Vec4::operator[](int index) {
+    switch(index) {
+        case 0: return x; break;
+        case 1: return y; break;
+        case 2: return z; break;
+        case 3: return w; break;
+    }
+    throw std::invalid_argument("Vector index out of bounds");
+}
+
+float Vec4::magnitude() {
+    return sqrt(magnitude_squared());
+}
+
+float Vec4::magnitude_squared() {
+    return dot(*this, *this);
+}
+
+Vec4 Vec4::normalize() {
+    return *this / magnitude();
+}
+
+float Vec4::dot(const Vec4& first, const Vec4& second) {
+    return first.x * second.x 
+         + first.y * second.y 
+         + first.z * second.z
+         + first.w * second.w;
+}
+
+}
---
+++ engine/math/Vec4.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <ostream>
+#include <math.h>
+
+#include "Vec3.h"
+
+namespace bh {
+
+class Vec4 {
+public:
+    Vec4(float, float, float, float);
+    Vec4(const Vec3& other, float);
+    Vec4(const Vec3& other);
+    Vec4();
+    
+    Vec4& operator =(const Vec4& other);
+    Vec4& operator =(const Vec3& other);
+
+    void operator +=(const Vec4& other);
+    void operator -=(const Vec4& other);
+    void operator *=(float scalar);
+    void operator /=(float scalar);
+
+    Vec4  operator +(const Vec4& other) const;
+    Vec4  operator -(const Vec4& other) const;
+    Vec4  operator *(float scalar) const;
+    Vec4  operator /(float scalar) const;
+    
+    float operator[](int index);
+
+    float magnitude();
+    float magnitude_squared();
+    Vec4  normalize();
+
+    static float dot(const Vec4& first, const Vec4& second);
+
+    friend std::ostream& operator<<(std::ostream& os, const Vec4& vec){
+        os << "<" << vec.x << ", " << vec.y << ", " << vec.z << ">";
+        return os;
+    }
+
+    float x, y, z, w;
+};
+
+}
