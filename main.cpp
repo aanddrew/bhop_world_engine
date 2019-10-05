54909790a5d3f51bc267129e6363d7c20ab5197a
Andrew Weller <andrewweller.cs@gmail.com>
2019-10-05 01:35:42 -0600

    main: testing cube.obj with bsp

--- main.cpp
+++ main.cpp
@@ -12,25 +12,19 @@ int main() {
         bh::Vec2(600.0f, 0.0f)
     );
 
-    bh::Tri3 weird_triangle {
-        bh::Vec3(0.0f, -2.0f, 7.0f),
-        bh::Vec3(-12.0f, -2.0f, 12.0f),
-        bh::Vec3(12.0f, -2.0f, 12.0f)
-    };
-
     bh::Player player;
     bh::PlayerController pc(&player);
 
     bh::Mat3 rotation = bh::Mat3::rotation_around_y(0.000001);
 
-    bh::Map map("map.obj");
+    bh::Map map("cube.obj");
 
     sf::Time dt;
     sf::Clock deltaClock;
     deltaClock.restart();
     while(window.isOpen()) {
         dt = deltaClock.restart();
-        std::cout << "fps: " << 1.0/dt.asSeconds() << std::endl;
+        //std::cout << "fps: " << 1.0/dt.asSeconds() << std::endl;
         bh::Mat4 translation = bh::Mat4::translate(0.7*dt.asSeconds(), 0, 0);
 
         sf::Event event;
@@ -55,16 +49,10 @@ int main() {
             sf::Mouse::setPosition(sf::Vector2i(window.getView().getSize().x/2,window.getView().getSize().y/2), window);
         //hide that mouse cursor
         window.setMouseCursorVisible(false);
+
+        //std::cout << player.get_camera().get_location() << std::endl;
         
-        //weird_triangle.c.z += 0.001;
-        //weird_triangle.apply_transform(rotation);
-        //weird_triangle.apply_transform(translation); //cam.rotate(0.000001, -0.000001); 
-        bh::Tri2 projections[3]; 
-        int num_triangles = bh::Camera::project_triangle(player.get_camera(), weird_triangle, projections); 
         window.clear(); 
-        for(int i = 0; i < num_triangles; i++) { 
-            bh::Tri2::draw_to_screen(projections[i], window); 
-        } 
         map.draw(player.get_camera(), window);
         window.display(); 
     } 
